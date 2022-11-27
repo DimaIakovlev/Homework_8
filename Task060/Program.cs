1db0b7b7ec5e404dd2abbd8c52d75c7f0c8aabd8
Yakovlev Dima <zif18@rambler.ru>
2022-11-27 23:14:36 +0500

    add Task060

---
+++ Task060/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+Console.Clear();
+Console.Write("Введите размеры трехмерного массива через пробел: ");
+string[] nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int[,,] array = CreateArray(new int[] { int.Parse(nums[0]), int.Parse(nums[1]), int.Parse(nums[2]) }, 10, 99);
+
+ShowArray(array);
+
+
+int[,,] CreateArray(int[] sizes, int min, int max)
+{
+    int[,,] randomArray = new int[sizes[0], sizes[1], sizes[2]];
+    for (int i = 0; i < randomArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < randomArray.GetLength(1); j++)
+        {
+            int k = 0;
+            while (k < randomArray.GetLength(2))
+            {
+                int element = new Random().Next(min, max + 1);
+                if (FindElement(randomArray, element)) continue;
+                randomArray[i, j, k] = element;
+                k++;
+            }
+
+        }
+
+    }
+    return randomArray;
+}
+
+bool FindElement(int[,,] array, int element)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                if (array[i, j, k] == element) return true;
+            }
+
+        }
+
+    }
+    return false;
+}
+
+void ShowArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]} ({i}, {j}, {k}) \t");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+
+}
